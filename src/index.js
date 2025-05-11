1f4b369e6ec37d58fcdb18e34164ac1ccaabf53a
SantosNovoa <novoasj8729@student.laccd.edu>
2025-05-11 00:34:03 -0400

    Got a working renderTodo function that appends todo list items to the todo-container in the HTML

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const projectModal = document.getElementById("newProjectModal");
 const projectCloseBtn = document.getElementById("project-close");
 const addProjectBtn = document.getElementById("new-project-btn");
 const closeBtnProject = document.getElementById("close-btn-project");
+const todoContainer = document.querySelector(".todo-container");
 
 
 window.onclick = function (event) {
@@ -42,3 +43,66 @@ addProjectBtn.onclick = function() {
 closeBtnProject.onclick = function() {
     projectModal.style.display = "none";
 }
+
+
+class TodoItem{
+    constructor(title, description, dueDate, priority, project) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.project = project;
+        this.id = crypto.randomUUID();
+    }
+}
+
+const firstTodo = new TodoItem("Gym", "I want to go to the gym and get strong", "05/12/25", "Medium", "Default");
+const secondTodo = new TodoItem("Read", "I want to finish the book Demon Haunted World", "05/29/25", "High", "Default");
+const thirdTodo = new TodoItem("Academic", "I want graduate with my Associate of Science", "06/05/2025", "High", "Default");
+console.log(firstTodo);
+
+
+function renderTodo (todo) {
+    const newTodoRow = document.createElement("div")
+    newTodoRow.className = "todo-row";
+    todoContainer.appendChild(newTodoRow);
+
+    newTodoRow.setAttribute("data-id", todo.id);
+
+    const title = document.createElement("div");
+    title.textContent = todo.title;
+    title.className = "title";
+    newTodoRow.appendChild(title);
+
+
+    const description = document.createElement("div");
+    description.textContent = todo.description;
+    description.className = "description";
+    newTodoRow.appendChild(description);
+
+    const dueDate = document.createElement("div");
+    dueDate.textContent = todo.dueDate;
+    dueDate.className = "due-date";
+    newTodoRow.appendChild(dueDate);
+
+    const priority = document.createElement("div");
+    priority.textContent = todo.priority;
+    priority.className = "priority";
+    newTodoRow.appendChild(priority);
+
+    const remove = document.createElement("i");
+    remove.className = "bi bi-trash";
+    // removeBtn.textContent = "🗑️";
+    const removeBtn = document.createElement("button");
+    removeBtn.className = "remove";
+    removeBtn.appendChild(remove);
+    newTodoRow.appendChild(removeBtn);
+
+    removeBtn.addEventListener("click", function () {
+        removeBook(todo.id);
+      });
+}
+
+renderTodo(firstTodo);
+renderTodo(secondTodo);
+renderTodo(thirdTodo);
