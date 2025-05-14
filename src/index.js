ed13a28b8353158bbc3008fdd8c29d5ac8201ee7
SantosNovoa <novoasj8729@student.laccd.edu>
2025-05-14 11:49:41 -0400

    Added a addProject function, renderProject function that allows the user to add projects onto the sidebar. The new projects are also appended as new dropdown options in the new todo form

--- src/index.js
+++ src/index.js
@@ -8,20 +8,42 @@ const todoCloseBtn = document.getElementById("todo-close");
 const closeBtnTodo = document.getElementById("close-btn-todo");
 const projectModal = document.getElementById("newProjectModal");
 const projectCloseBtn = document.getElementById("project-close");
-const addProjectBtn = document.getElementById("new-project-btn");
+const addProjectBtn = document.getElementById("project-btn");
 const closeBtnProject = document.getElementById("close-btn-project");
 const todoContainer = document.querySelector(".todo-container");
 const titleValue = document.getElementById("title");
 const descriptionValue = document.getElementById("description");
 const dueDateValue = document.getElementById("dueDate");
 const priorityValue = document.getElementById("priority");
-
+const projectValue = document.getElementById("taskProject");
+const titleDiv = document.querySelector(".title-div")
+const descriptionDiv = document.querySelector(".description-div");
+const dueDateDiv = document.querySelector(".due-date-div");
+const priorityDiv = document.querySelector(".priority-div");
+const projectDiv = document.querySelector(".project-div");
+const projectContainer = document.querySelector(".project-container");
+const projectBtn = document.getElementById("project-btn");
+const projectName = document.getElementById("projectName");
 const todoForm = document.getElementById("newTodo");
+const dropdownProject = document.getElementById("taskProject");
 todoForm.addEventListener("submit", submitTodoForm);
+
+const projectForm = document.getElementById("newProjectForm");
+projectForm.addEventListener("submit", submitProjectForm);
+
 
 console.log(todoForm);
 
-window.onclick = function (event) {
+const sidebar = document.querySelector(".sidebar");
+const container = document.querySelector(".content-container");
+document
+  .getElementById("sidebar-toggle")
+  .addEventListener("click", () => {
+    sidebar.classList.toggle("closed");
+    container.classList.toggle("sidebar-collapsed");
+  });
+
+window.onclick = function(event) {
   if (event.target === todoModal) {
     todoModal.style.display = "none";
   } else if (event.target === projectModal) {
@@ -29,27 +51,31 @@ window.onclick = function (event) {
   }
 };
 
-addTodoBtn.onclick = function () {
+projectBtn.onclick = function() {
+  projectModal.style.display = "none";
+}
+
+addTodoBtn.onclick = function() {
   todoModal.style.display = "block";
 };
 
-todoCloseBtn.onclick = function () {
+todoCloseBtn.onclick = function() {
   todoModal.style.display = "none";
 };
 
-closeBtnTodo.onclick = function () {
+closeBtnTodo.onclick = function() {
   todoModal.style.display = "none";
 };
 
-projectCloseBtn.onclick = function () {
+projectCloseBtn.onclick = function() {
   projectModal.style.display = "none";
 };
 
-addProjectBtn.onclick = function () {
+addProjectBtn.onclick = function() {
   projectModal.style.display = "block";
 };
 
-closeBtnProject.onclick = function () {
+closeBtnProject.onclick = function() {
   projectModal.style.display = "none";
 };
 
@@ -61,36 +87,38 @@ class TodoItem {
     this.priority = priority;
     this.project = project;
     this.id = crypto.randomUUID();
+    this.completed = false;
   }
 }
+
 
 const firstTodo = new TodoItem(
   "Need to start going to the Gym",
   "I want to go to the gym and get strong",
   "2025-05-12",
   "Medium",
-  "Default"
+  "Inbox"
 );
 const secondTodo = new TodoItem(
   "I want to read more books",
   "I want to finish the book Demon Haunted World",
   "2025-05-29",
   "High",
-  "Default"
+  "Inbox"
 );
 const thirdTodo = new TodoItem(
   "Get that associates degree to transfer to a 4-year",
   "I want graduate with my Associate of Science",
   "2025-06-05",
   "High",
-  "Default"
+  "Inbox"
 );
 const fourthTodo = new TodoItem(
   "I want to play some videogames over the summer",
   "I want to play a lot of games",
   "2025-05-06",
   "Low",
-  "Default"
+  "Inbox"
 );
 console.log(firstTodo);
 
@@ -99,8 +127,6 @@ function addTodo(title, description, dueDate, priority, project) {
     const newTodo = new TodoItem(title, description, dueDate, priority, project);
     renderTodo(newTodo);
 }
-
-addTodo("I want to go on long walks on the beach", "It's always been a dream of mine I want to go on long walks with Loki & Tyr", "2012-03-02", "High", "Default");
 
 function renderTodo(todo) {
   const newTodoRow = document.createElement("div");
@@ -124,26 +150,24 @@ function renderTodo(todo) {
   const title = document.createElement("div");
   title.textContent = todo.title;
   title.className = "title";
-  // newTodoRow.appendChild(title);
   leftSide.appendChild(title);
 
   const description = document.createElement("div");
   description.textContent = todo.description;
   description.className = "description";
-  // newTodoRow.appendChild(description);
-  // leftSide.appendChild(description);
+
   newTodoRow.appendChild(leftSide);
 
   const dueDate = document.createElement("div");
   dueDate.textContent = todo.dueDate;
   dueDate.className = "due-date";
-  // newTodoRow.appendChild(dueDate);
+
   rightSide.appendChild(dueDate);
 
   const priority = document.createElement("div");
   priority.textContent = todo.priority;
   priority.className = "priority";
-  // newTodoRow.appendChild(priority);
+
 
   // checking what the user entered as a priority
   if (priority.textContent == "High") {
@@ -160,7 +184,91 @@ function renderTodo(todo) {
   removeBtn.className = "remove";
   removeBtn.appendChild(remove);
   rightSide.appendChild(removeBtn);
+
+
+
+  const info = document.createElement("i");
+  info.className = "bi bi-info-circle";
+  const infoBtn = document.createElement("button");
+  infoBtn.className = "info";
+  infoBtn.appendChild(info);
+  rightSide.appendChild(infoBtn)
   newTodoRow.appendChild(rightSide);
+
+  const infoModal = document.getElementById("infoModal");
+  const infoModalBody = document.getElementById("info-modal-body");
+  const infoClose = document.getElementById("info-close");
+  const closeInfoBtn = document.getElementById("close-btn-task-info");
+  
+ 
+  closeInfoBtn.onclick = function() {
+    infoModal.style.display = "none";
+  }
+
+  infoClose.onclick = function() {
+    infoModal.style.display = "none";
+  }
+
+  infoBtn.onclick = function() {
+    infoModal.style.display = "block";
+
+    
+    //clear previous contents of the div's
+    titleDiv.innerHTML = "";
+    descriptionDiv.innerHTML = "";
+    dueDateDiv.innerHTML = "";
+    priorityDiv.innerHTML = "";
+    projectDiv.innerHTML = "";
+
+    //title content
+    const titleLabelSpan = document.createElement("span");
+    titleLabelSpan.textContent = "Title: ";
+    titleLabelSpan.className = "info-span";
+    titleDiv.appendChild(titleLabelSpan);
+    const titleSpan = document.createElement("span");
+    titleSpan.className = "info-task";
+    titleSpan.textContent = todo.title;
+    titleDiv.appendChild(titleSpan);
+
+    //description content
+    const descriptionLabelSpan = document.createElement("span");
+    descriptionLabelSpan.textContent = "Description: ";
+    descriptionLabelSpan.className = "info-span";
+    descriptionDiv.appendChild(descriptionLabelSpan);
+    const descriptionSpan = document.createElement("span");
+    descriptionSpan.className = "info-task desc";
+    descriptionSpan.textContent = todo.description;
+    descriptionDiv.appendChild(descriptionSpan);
+
+    //dueDate content
+    const dueDateLabelSpan = document.createElement("span");
+    dueDateLabelSpan.textContent = "Due date: ";
+    dueDateLabelSpan.className = "info-span";
+    dueDateDiv.appendChild(dueDateLabelSpan);
+    const dueDateSpan = document.createElement("span");
+    dueDateSpan.className = "info-task";
+    dueDateSpan.textContent = todo.dueDate;
+    dueDateDiv.appendChild(dueDateSpan);
+
+    //priority content
+    const priorityLabelSpan = document.createElement("span");
+    priorityLabelSpan.textContent = "Priority: ";
+    priorityLabelSpan.className = "info-span";
+    priorityDiv.appendChild(priorityLabelSpan);
+    const prioritySpan = document.createElement("span");
+    prioritySpan.className = "info-task";
+    prioritySpan.textContent = todo.priority;
+    priorityDiv.appendChild(prioritySpan);
+
+    const projectLabelSpan = document.createElement("span");
+    projectLabelSpan.textContent = "Project: ";
+    projectLabelSpan.className = "info-span";
+    projectDiv.appendChild(projectLabelSpan);
+    const projectSpan = document.createElement("span");
+    projectSpan.className = "info-task";
+    projectSpan.textContent = todo.project;
+    projectDiv.appendChild(projectSpan);
+  }
 
   //remove button event listener
   removeBtn.addEventListener("click", function () {
@@ -174,13 +282,17 @@ function renderTodo(todo) {
       complete.classList.replace("bi-circle", "bi-check-circle");
       title.classList.add("task-done-text");
       newTodoRow.style.backgroundColor = "#d8d8d8";
+      todo.completed = true;
     } else {
       complete.classList.replace("bi-check-circle", "bi-circle");
       title.classList.remove("task-done-text");
       newTodoRow.style.backgroundColor = "#fff";
+      todo.completed = false;
     }
   });
 }
+
+
 
 function removeTodo(todoId) {
   const rowToRemove = document.querySelector(`.todo-row[data-id="${todoId}"]`);
@@ -188,7 +300,6 @@ function removeTodo(todoId) {
     rowToRemove.remove();
   }
 }
-
 function submitTodoForm(e) {
     e.preventDefault();
 
@@ -201,11 +312,25 @@ function submitTodoForm(e) {
     const description = descriptionValue.value;
     const dueDate = dueDateValue.value;
     const priority = priorityValue.value;
-    const project = "Default";
+    const project = projectValue.value;
 
     addTodo(title, description, dueDate, priority, project);
     todoForm.reset();
     todoModal.style.display = "none";
+}
+
+function submitProjectForm(e) {
+  e.preventDefault();
+
+  if (!projectForm.checkValidity()) {
+    projectForm.reportValidity();
+    return;
+  }
+
+  const project = projectName.value;
+  addProject(project);
+  projectForm.reset();
+  projectModal.style.display = "none";
 }
 
 
@@ -213,3 +338,29 @@ renderTodo(firstTodo);
 renderTodo(secondTodo);
 renderTodo(thirdTodo);
 renderTodo(fourthTodo);
+
+const projects = {
+  Default: []
+};
+
+
+function addProject(name) {
+  if (!projects[name]) {
+    projects[name] = [];
+    renderProject(name);
+
+  }
+}
+
+
+function renderProject(projectName) {
+  const projectItem = document.createElement("div");
+  projectItem.textContent = projectName;
+  projectItem.classList.add("project-item");
+  projectContainer.appendChild(projectItem);
+
+  const dropdownItem = document.createElement("option")
+  dropdownItem.value = projectName;
+  dropdownItem.textContent = projectName;
+  dropdownProject.appendChild(dropdownItem);
+}
